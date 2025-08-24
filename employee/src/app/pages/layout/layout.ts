86ede9d7cff3cdf9b928fa090e90b4105bb5adc4
ArandasSheikh <aranadassheikh@gmail.com>
2025-08-24 09:47:14 +0500

    Make layout component

--- employee/src/app/pages/layout/layout.ts
+++ employee/src/app/pages/layout/layout.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
 export class Layout {
+  constructor(private router: Router) {}
+
+  logout() {
+    alert("You have logged out!");
+    this.router.navigate(['/login']); // redirect to dashboard
+  }
 
 }
